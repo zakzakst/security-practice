cf3f8f36ad53c2b9a5782011f900fd7ca8385ec6
zakzakst <e1433104@gmail.com>
2023-08-11 16:33:21 +0900

    06

--- 03/server.js
+++ 03/server.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto')
 const express = require('express')
 const api = require('./routes/api')
+const csrf = require('./routes/csrf')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.use('/api', api)
+app.use('/csrf', csrf)
 
 app.get('/', (req, res, next) => {
   res.end('Top Page')
